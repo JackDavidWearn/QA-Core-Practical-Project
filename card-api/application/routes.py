81d0ab9478515f16968ae744d440487385a5606a
Jack Wearn <wearnjack@gmail.com>
2022-03-31 13:35:03 +0100

    Changes

--- card-api/application/routes.py
+++ card-api/application/routes.py
@@ -3,10 +3,10 @@ from flask import Flask, request, Response
 
 @app.route('/get-card', methods=['POST'])
 def card():
-    # cards = request.get_json()
-    # symbol = cards["value"]
-    # suit = cards["suit"]
-    # values = {'A':'Ace', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eight', '9':'Nine', '10':'Ten', 'J':'Jack', 'Q':'Queen', 'K':'King'}
-    # value = values[symbol]
-    # return jsonify({"value":value, "suit":suit})
+    cards = request.get_json()
+    symbol = cards["value"]
+    suit = cards["suit"]
+    values = {'A':'Ace', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eight', '9':'Nine', '10':'Ten', 'J':'Jack', 'Q':'Queen', 'K':'King'}
+    value = values[symbol]
+    return jsonify({"value":value, "suit":suit})
     
--- cardvalue-api/application/routes.py
+++ cardvalue-api/application/routes.py
@@ -3,12 +3,10 @@ from flask import Flask, request, Response
 import random
 
 # Declaring the card deck values
-# symbols = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+symbols = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 # Route to get a random card deck suit
 @app.route('/get-values', methods=['GET'])
 def get_values():
-    # symbol = choice(symbols)
-    # return Response(symbol)
-    symbols = random.choice(['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'])
-    return Response(symbols, mimetype='text/plain')
+    symbol = choice(symbols)
+    return Response(symbol)
