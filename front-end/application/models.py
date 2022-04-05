0af84db9fffe7cd131e88d93ba5e8e1acb05a08a
Jack Wearn <wearnjack@gmail.com>
2022-04-05 20:38:28 +0100

    Added changes to database

--- front-end/application/models.py
+++ front-end/application/models.py
@@ -4,6 +4,7 @@ class Cards(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cards_value = db.Column(db.String(50))
     cards_suit = db.Column(db.String(50))
+    date_generated = db.Column(db.DateTime)
 
     def __str__(self):
         return f"{self.cards_value} of {self.cards_suit}"
