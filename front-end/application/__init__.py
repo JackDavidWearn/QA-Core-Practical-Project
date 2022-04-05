66932166aae6ef22548dd47059290ab682be6f1f
Jack Wearn <wearnjack@gmail.com>
2022-04-05 15:17:40 +0100

    Changes

--- front-end/application/__init__.py
+++ front-end/application/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/carddb"
+# app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/carddb"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carddb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
