10034ade755db4dd4b8ae70f5061bf30383d91a4
Jack Wearn <wearnjack@gmail.com>
2022-04-05 12:47:11 +0100

    Changes

--- front-end/application/models.py
+++ front-end/application/models.py
@@ -1,10 +1,9 @@
 from application import db
 
 class Cards(db.Model):
-
     pk_cards_id = db.Column(db.Integer, primary_key=True)
     cards_value = db.Column(db.String(50))
     cards_suit = db.Column(db.String(50))
 
-    def __repr__(self):
+    def __str__(self):
         return f"{self.cards_value} of {self.cards_suit}"
