e8818555a2dbb8fb28e21f5b55febb7abfe3b7ac
Jack Wearn <wearnjack@gmail.com>
2022-04-05 11:22:29 +0100

    Changes

--- cardsuit-api/application/routes.py
+++ cardsuit-api/application/routes.py
@@ -1,11 +1,11 @@
 from application import app
 from flask import Response
-import random
+from random import choice
 
 # Route to get a random card deck suit
 @app.route('/get-suit', methods=['GET'])
 def get_suit():
     # Declaring the 4 card deck suits
     suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    suit = random.choice(suits)
+    suit = choice(suits)
     return Response(suit, mimetype="text/plain")
--- cardvalue-api/application/routes.py
+++ cardvalue-api/application/routes.py
@@ -1,11 +1,11 @@
 from application import app
 from flask import Response
-import random 
+from random import choice
 
 # Route to get a random card deck suit
 @app.route('/get-values', methods=['GET'])
 def get_values():
     # Declaring the card deck values
     symbols = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    symbol = random.choice(symbols)
+    symbol = choice(symbols)
     return Response(symbol, mimetype="text/plain")
