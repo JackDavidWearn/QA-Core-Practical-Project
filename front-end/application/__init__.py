5e7e0b368cdf6fa774376615c862ec66771fed7b
Jack Wearn <wearnjack@gmail.com>
2022-04-05 15:14:13 +0100

    Changes

--- front-end/application/__init__.py
+++ front-end/application/__init__.py
@@ -3,6 +3,7 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/carddb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
